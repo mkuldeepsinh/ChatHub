0e06f1ecdfff15813555ce6671ba6792fbb69da6
Kuldeepsinh Makwana <mkuldeepsinh9@gmail.com>
2025-07-18 15:41:34 +0530

    Server chnage for the production

--- server/server.js
+++ server/server.js
@@ -13,6 +13,8 @@ import { sendMessage, markAsRead } from "./controllers/msg.controller.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { fileURLToPath } from "url";
+import path from "path";
 
 dotenv.config();
 dbConnect();
@@ -31,6 +33,17 @@ app.use(cors({
 app.use("/api/v1/user" , userRouter);
 app.use("/api/v1/chats" , chatRouter);
 app.use("/api/v1/message" , msgRouter);
+
+// --- Place the production static serving code here ---
+if (process.env.NODE_ENV === 'production') {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
+}
 
 // Create HTTP server and bind Express app
 const server = http.createServer(app);
